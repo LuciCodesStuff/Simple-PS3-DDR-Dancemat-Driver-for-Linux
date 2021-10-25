1a7180bfb3584041d7883dfaeb53922bea258684
Robert Lofaso <robert.lofaso@gmail.com>
2021-10-25 19:58:55 -0400

    Update usb_ps3_dancemat_driver.c
    Corrected mappings, code cleanup.

--- usb_ps3_dancemat_driver.c
+++ usb_ps3_dancemat_driver.c
@@ -24,20 +24,14 @@ If there is anything in this code that can cause harm to your system,
     You have been warned - I don't know a struct from an object.
 /!\
 
-BIG WARNING!
-I may change the mappings! I started with the lowest bits and worked my way up.
-Future versions may require you to remap your games!
-I'm going to evaluate how the xbox mat driver maps hits and mimic it. Ideally I want
-this to work out of the box with the least amount of end user configuration,
-otherwise I've failed you, the person reading my stupid comments.
-
 TODO:
-Clean up
-Confirm dependancies - too many includes
-Test against major distros
+INPROGRESS-Confirm dependancies - too many includes
+TOOLAZY-Test against major distros
 
 CHANGELOG:
-2021-10-22 - Initial release following code cleanup
+2021-10-22 - 	Initial release following code cleanup
+2021-10-25 - 	Changed button mappings to use dpad, cardinal, and start/select/mode
+		No longer using analog for dpad
 
 TIPS:
 I don't want them. This is like 20 lines of stackoverflow code.
@@ -46,23 +40,9 @@ https://my.fsf.org/donate <== They could use it!
 
 #define HID_VENDOR_ID       ( 0x1ccf )
 #define HID_PRODUCT_ID      ( 0x1010 )
-#define NUMBER_OF_BUTTONS   11
+//#define NUMBER_OF_BUTTONS   11
 
 static struct hid_driver hid_spsdm; // [S]imple [P]lay[S]tation [D]ance[M]at driver. Find/Replace if you don't like it.
-
-// Kept if needed in the future.
-/*
-static int __check_hid_spsdm(struct device_driver *drv, void *data)
-{
-	struct hid_driver *hdrv = to_hid_driver(drv);
-	struct hid_device *hdev = data;
-
-	if (hdrv == &hid_spsdm)
-		return 0;
-
-	return hid_match_device(hdev, hdrv) != NULL;
-}
-*/
 
 static bool hid_spsdm_match(struct hid_device *hdev,
 			      bool ignore_special_driver)
@@ -104,19 +84,24 @@ static int hid_spsdm_input_configured(struct hid_device *dev,
                                 struct hid_input *input)
 {
     struct input_dev * input_dev = input->input;
-    int i;
 
     hid_set_drvdata(dev, input_dev);
 
     set_bit(EV_KEY, input_dev->evbit);
     //set_bit(EV_ABS, input_dev->evbit); // Eventually I want the analog and digital hits to return -- for completion sake.
 
-    for (i = 0; i < NUMBER_OF_BUTTONS; i++) {
-        set_bit(BTN_TRIGGER_HAPPY1 + i, input_dev->keybit);
-        // Using BTN_TRIGGER_HAPPY here instead of BTN_TRIGGER. This seems to get better results, but shouldn't?
-        // It might have been because my original attempts mapped 24 buttons, most were empty for debugging/learning.
-        // If there are any issues with using BTN_TRIGGER_HAPPY please let me know and I will update. Otherwise it ain't broke.
-    }
+    // Set the dpad and cardinal bits
+    set_bit(BTN_DPAD_UP, input_dev->keybit);
+    set_bit(BTN_DPAD_DOWN, input_dev->keybit);
+    set_bit(BTN_DPAD_LEFT, input_dev->keybit);
+    set_bit(BTN_DPAD_RIGHT, input_dev->keybit);
+    set_bit(BTN_NORTH, input_dev->keybit);      // TRIANGLE
+    set_bit(BTN_SOUTH, input_dev->keybit);      // X
+    set_bit(BTN_EAST, input_dev->keybit);       // CIRCLE
+    set_bit(BTN_WEST, input_dev->keybit);       // SQUARE
+    set_bit(BTN_START, input_dev->keybit);
+    set_bit(BTN_SELECT, input_dev->keybit);
+    set_bit(BTN_MODE, input_dev->keybit);       // PS Button
 
     return 0;
 }
@@ -125,38 +110,43 @@ static int hid_spsdm_raw_event(struct hid_device *dev,
                          struct hid_report *report, u8 *data, int len)
 {
     struct input_dev *input_dev = hid_get_drvdata(dev);
-    int ret; // Not used?
-    //int idx; // not used - no longer looping indexes
-    //int btn; // not used - no longer looping buttons
-    //int i; // not used - no longer looping... loops
+    //int ret;
 
     /*
     Indexes:
     [0] holds button presses
     [1] holds extra keys
-    [3] holds dpad movements, but I am unable to parse this correctly at this time
-        This data seems to be locked at 0x08 when not in use. It appears the dpad digital hits go low, where as the btn hits go high
-        Once I get this sorted out I will change this over.
+    [2] holds dpad movements
+	unlike the button hits, this returns 0x08 and goes low when the dpad is press.
+	subtract this value from 0x08 and parse like a button.
     [7] - [10] are anaglog returns of the "left stick", which can be used in place of the dpad for our uses.
-    Note that the pad will return seperate analog data for each direction, so left and right, or up and down work.
-    I plan on using the dpad return once I figure it out, not because the current method doesn't work but because I believe
-    it's the correct way to make this driver. Mapping the analog to digital is really a hack.
-    __I refuse to make this a loop. Fight me.__
     */
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY1, data[0] & 0x01); // SQUARE
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY2, data[0] & 0x02); // X
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY3, data[0] & 0x04); // O
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY4, data[0] & 0x08); // TRIANGLE
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY5, data[1] & 0x01); // SELECT
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY6, data[1] & 0x02); // START
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY7, data[1] & 0x10); // PS Button
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY8, data[7] & 0xFF); // RIGHT
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY9, data[8] & 0xFF); // LEFT
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY10, data[9] & 0xFF); // UP
-    input_report_key(input_dev, BTN_TRIGGER_HAPPY11, data[10] & 0xFF); // DOWN
+
+    //Correct mappings AFAIK
+    input_report_key(input_dev, BTN_WEST, data[0] & 0x01);      // SQUARE
+    input_report_key(input_dev, BTN_SOUTH, data[0] & 0x02);     // X
+    input_report_key(input_dev, BTN_EAST, data[0] & 0x04);      // CIRCLE
+    input_report_key(input_dev, BTN_NORTH, data[0] & 0x08);     // TRIANGLE
+    input_report_key(input_dev, BTN_SELECT, data[1] & 0x01);    // SELECT
+    input_report_key(input_dev, BTN_START, data[1] & 0x02);     // START
+    input_report_key(input_dev, BTN_MODE, data[1] & 0x10);      // PS Button
+
+    //Former analog to digital mappings. Works, but not right!
+    input_report_key(input_dev, BTN_DPAD_RIGHT, data[7] & 0xFF);	// RIGHT
+    input_report_key(input_dev, BTN_DPAD_LEFT, data[8] & 0xFF);		// LEFT
+    input_report_key(input_dev, BTN_DPAD_UP, data[9] & 0xFF);		// UP
+    input_report_key(input_dev, BTN_DPAD_DOWN, data[10] & 0xFF);	// DOWN
+
+    //To many issues trying to get the bits right on these attempts. Keep trying....
+    //input_report_key(input_dev, BTN_DPAD_RIGHT, (0x08 - data[2]) & 0x06);    // RIGHT
+    //input_report_key(input_dev, BTN_DPAD_LEFT, (0x08 - data[2]) & 0x02);     // LEFT
+    //input_report_key(input_dev, BTN_DPAD_UP, (0x08 - data[2]) & 0x08);       // UP
+    //input_report_key(input_dev, BTN_DPAD_DOWN, (0x08 - data[2]) & 0x04);     // DOWN
+
     input_sync(input_dev);
 
-    return ret; // ret = NULL? wtf? I think I got some reading to do.... fml....
+    //return ret;
+    return 0; // Meh
 }
 
 static const struct hid_device_id hid_table[] = {
